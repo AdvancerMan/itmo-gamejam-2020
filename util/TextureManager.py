0c498894f4370b4347715ed7107e1046a63b232d
AdvancerMan <advancerman@gmail.com>
2020-04-23 11:47:41 +0700

    Added animations to TextureManager

--- util/TextureManager.py
+++ util/TextureManager.py
@@ -1,20 +1,30 @@
 import pygame as pg
+import pyganim as pga
 from util.Logger import log
+from util.Textures import TextureInfo, AnimationInfo
 
 
 class TextureManager:
     def __init__(self):
         self.__textures = {}
 
-    # textureID should be taken from Textures.py
-    def getTexture(self, textureID: str) -> pg.Surface:
-        texture = self.__textures.get(textureID, None)
+    def getAnimation(self, animationInfo: AnimationInfo) -> pga.PygAnimation:
+        return pga.PygAnimation(list(map(
+            lambda pathToDuration: (self.__getTexture(pathToDuration[0]), pathToDuration[1]),
+            animationInfo.value
+        )))
+
+    def getTexture(self, textureInfo: TextureInfo) -> pg.Surface:
+        return self.__getTexture(textureInfo.value)
+
+    def __getTexture(self, path: str) -> pg.Surface:
+        texture = self.__textures.get(path, None)
         if texture is None:
             try:
-                texture = pg.image.load(textureID)
+                texture = pg.image.load(path)
             except pg.error:
-                log("Error occured during loading " + textureID)
+                log("Error occurred during loading " + path)
                 texture = pg.Surface((20, 20))
                 texture.fill((128, 0, 128))  # purple
-            self.__textures[textureID] = texture
-        return texture
+            self.__textures[path] = texture
+        return texture.copy()
--- util/Textures.py
+++ util/Textures.py
@@ -1,5 +1,20 @@
 from enum import Enum
+from os.path import join
 
 
-class Textures(Enum):
-    BACKGROUND = "background.png"
+def _createPic(*picPath: str):
+    return join(*picPath) + ".png"
+
+
+def _createAnimation(frameCount: int, framesDuration: list, *picsPath: str):
+    picsPath = join(*picsPath)
+    return list(zip([_createPic(picsPath + str(i)) for i in range(1, frameCount + 1)], framesDuration))
+
+
+class TextureInfo(Enum):
+    BACKGROUND = _createPic("background")
+
+
+class AnimationInfo(Enum):
+    PLAYER_ANIMATION = _createAnimation(5, [100] * 5, "player")
+
