7a6edba674730bd6545d7845107aa231ff915bb6
AdvancerMan <advancerman@gmail.com>
2020-04-24 23:14:04 +0700

    Added error handling in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,17 @@
-import pygame as pg
-from game.Game import Game
-from process.MenuProcess import MenuProcess
+try:
+    import pygame as pg
+    from game.Game import Game
+    from process.MenuProcess import MenuProcess
 
-pg.init()
-game = Game()
-game.run(MenuProcess(game))
-pg.quit()
+    pg.init()
+    game = Game()
+    game.run(MenuProcess(game))
+    pg.quit()
+except:
+    try:
+        from traceback import format_exc
+        from util.Logger import log
+
+        log(format_exc())
+    except:
+        print("ERROR IN LOGGING")
