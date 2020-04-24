196153dc8522dcd6d62f74d1024616a436ee5a4c
AdvancerMan <advancerman@gmail.com>
2020-04-25 00:14:59 +0700

    Optimized animation in TextureManager (now it's taken from sprite sheets)

--- util/TextureManager.py
+++ util/TextureManager.py
@@ -2,6 +2,7 @@ import pygame as pg
 import pyganim as pga
 from util.Logger import log
 from util.Textures import TextureInfo, AnimationInfo
+from traceback import format_exc
 
 
 class TextureManager:
@@ -9,22 +10,24 @@ class TextureManager:
         self.__textures = {}
 
     def getAnimation(self, animationInfo: AnimationInfo) -> pga.PygAnimation:
-        return pga.PygAnimation(list(map(
-            lambda pathToDuration: (self.__getTexture(pathToDuration[0]), pathToDuration[1]),
-            animationInfo.value
-        )))
+        path, loadTexture, duration = animationInfo.value
+        textures = self.__getTexture(path, loadTexture)
+        if len(textures) != len(duration):
+            textures = textures * len(duration)
+        return pga.PygAnimation(list(zip(textures, duration)))
 
     def getTexture(self, textureInfo: TextureInfo) -> pg.Surface:
-        return self.__getTexture(textureInfo.value)
+        return self.__getTexture(textureInfo.value, lambda: pg.image.load(textureInfo.value))[0]
 
-    def __getTexture(self, path: str) -> pg.Surface:
+    def __getTexture(self, path: str, loadTexture) -> list:  # list of pg.Surface:
         texture = self.__textures.get(path, None)
         if texture is None:
             try:
-                texture = pg.image.load(path)
+                texture = loadTexture()
             except pg.error:
-                log("Error occurred during loading " + path)
-                texture = pg.Surface((20, 20))
-                texture.fill((128, 0, 128))  # purple
+                log("Error occurred during loading %s:" % path)
+                log(format_exc())
+                texture = [pg.Surface((20, 20))]
+                texture[0].fill((128, 0, 128))  # purple
             self.__textures[path] = texture
-        return texture.copy()
+        return texture
--- util/Textures.py
+++ util/Textures.py
@@ -1,20 +1,21 @@
 from enum import Enum
 from os.path import join
+import pyganim as pga
 
 
-def _createPic(*picPath: str):
-    return join(*picPath) + ".png"
+def _createPic(*picPath: str) -> str:
+    return join(*picPath)
 
 
-def _createAnimation(frameCount: int, framesDuration: list, *picsPath: str):
-    picsPath = join(*picsPath)
-    return list(zip([_createPic(picsPath + str(i)) for i in range(1, frameCount + 1)], framesDuration))
+def _createAnimation(rows: int, columns: int, framesDuration: list, *picsPath: str):  # -> (str, function, list<int>)
+    assert rows * columns == len(framesDuration)
+    picsPath = _createPic(*picsPath)
+    return picsPath, lambda: pga.getImagesFromSpriteSheet(picsPath, rows=rows, cols=columns, rects=[]), framesDuration
 
 
 class TextureInfo(Enum):
-    BACKGROUND = _createPic("background")
+    BACKGROUND = _createPic("background.png")
 
 
 class AnimationInfo(Enum):
-    PLAYER_ANIMATION = _createAnimation(5, [100] * 5, "player")
-
+    FRIEND_ANIMATION = _createAnimation(2, 3, [100] * 6, "pics", "friend.png")
