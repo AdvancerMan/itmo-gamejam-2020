aaf0996d0999dffe5cf84103f4e40128def44a2e
AdvancerMan <advancerman@gmail.com>
2020-05-01 19:11:45 +0700

    Fixed acting in Player

--- objects/friendly/Player.py
+++ objects/friendly/Player.py
@@ -1,37 +1,30 @@
 import pygame as pg
 from Box2D import *
 from game.Game import Game
-from objects.base.InGameObject import InGameObject
+from objects.base.ActiveObject import ActiveObject
 from util.textures.Textures import AnimationInfo
 
 
-class Player(InGameObject):
+class Player(ActiveObject):
     def __init__(self, game: Game, process):
         # process: GameProcess
-        InGameObject.__init__(self, game, process,
+        ActiveObject.__init__(self, game, process,
                               game.getTextureManager().getAnimation(AnimationInfo.PLAYER_ANIMATION),
-                              process.getFactory().createRectangleBody(self, b2_dynamicBody, 40, 100))
+                              process.getFactory().createRectangleBody(self, b2_dynamicBody, 40, 100),
+                              200, 400)
+        self.__actions = set()
 
     def update(self):
         for e in self.process.getEvents():
-            if e.type == pg.KEYDOWN:
+            if e.type == pg.KEYDOWN or e.type == pg.KEYUP:
+                act = self.__actions.add if e.type == pg.KEYDOWN else self.__actions.discard
                 if e.key == pg.K_d:
-                    self.goRight()
+                    act("goRight")
                 elif e.key == pg.K_a:
-                    self.goLeft()
+                    act("goLeft")
                 elif e.key == pg.K_w:
-                    self.jump()
+                    act("jump")
                 elif e.key == pg.K_e:
-                    self.shoot()
-
-    def goLeft(self):
-        pass
-
-    def goRight(self):
-        pass
-
-    def jump(self):
-        pass
-
-    def shoot(self):
-        pass
+                    act("shoot")
+        for action in self.__actions:
+            getattr(self, action)()
