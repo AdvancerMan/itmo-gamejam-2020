9fa6c5c995766c0d5a04998257eb5fd17c953e39
AdvancerMan <advancerman@gmail.com>
2020-05-02 14:46:50 +0700

    Fixed bug in ContactListener

--- util/box2d/ContactListener.py
+++ util/box2d/ContactListener.py
@@ -1,24 +1,22 @@
 from Box2D import *
 
-from objects.base.InGameObject import InGameObject
 
-
-def callOnBoth(contact: b2Contact, f, *args):
+def callOnBoth(contact: b2Contact, fName: str, *args):
     objA = contact.fixtureA.body.userData
     objB = contact.fixtureB.body.userData
-    f(objA, objB, contact, *args)
-    f(objB, objA, contact, *args)
+    getattr(objA, fName)(objB, contact, *args)
+    getattr(objB, fName)(objA, contact, *args)
 
 
 class ContactListener(b2ContactListener):
     def BeginContact(self, contact: b2Contact):
-        callOnBoth(contact, InGameObject.beginContact)
+        callOnBoth(contact, "beginContact")
 
     def EndContact(self, contact: b2Contact):
-        callOnBoth(contact, InGameObject.endContact)
+        callOnBoth(contact, "endContact")
 
     def PreSolve(self, contact: b2Contact, oldManifold: b2Manifold):
-        callOnBoth(contact, InGameObject.preSolve, oldManifold)
+        callOnBoth(contact, "preSolve", oldManifold)
 
     def PostSolve(self, contact: b2Contact, impulse: b2ContactImpulse):
-        callOnBoth(contact, InGameObject.postSolve, impulse)
+        callOnBoth(contact, "postSolve", impulse)
