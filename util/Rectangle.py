cf2b774d3fd37126c75305963abcf950106c9a6e
AdvancerMan <advancerman@gmail.com>
2020-05-02 11:54:28 +0700

    Added intersects() method to Rectangle

--- util/Rectangle.py
+++ util/Rectangle.py
@@ -39,6 +39,11 @@ class Rectangle:
     def __str__(self):
         return str((self.x, self.y, self.w, self.h))
 
+    def intersects(self, other) -> bool:
+        # other: Rectangle
+        return intersects(self.x, self.x + self.w, other.x, other.x + other.w) \
+               and intersects(self.y, self.y + self.h, other.y, other.y + other.h)
+
 
 def rectFromTwoPoints(x1: float, y1: float, x2: float, y2: float) -> Rectangle:
     return Rectangle(x1, y1, x2 - x1, y2 - y1)
@@ -46,3 +51,7 @@ def rectFromTwoPoints(x1: float, y1: float, x2: float, y2: float) -> Rectangle:
 
 def rectFromSize(x: float, y: float, width: float, height: float) -> Rectangle:
     return Rectangle(x, y, width, height)
+
+
+def intersects(s1: float, f1: float, s2: float, f2: float) -> bool:
+    return max(s1, s2) <= min(f1, f2)
