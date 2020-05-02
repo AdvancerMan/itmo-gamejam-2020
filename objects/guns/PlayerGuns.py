9133ba3b02ad73e7c15580620fbf1000ef033055
AdvancerMan <advancerman@gmail.com>
2020-05-02 20:53:34 +0700

    Resolved conflicts

--- objects/guns/PlayerGuns.py
+++ objects/guns/PlayerGuns.py
@@ -2,6 +2,7 @@ import pyganim as pga
 from Box2D import *
 from game.Game import Game
 from objects.guns.Gun import Gun
+from util.box2d.BodyFactory import createRectangleBodyTemplate
 from util.textures.Textures import AnimationInfo, AnimationPackInfo
 
 
@@ -9,7 +10,7 @@ class UsualGun(Gun):
     def __init__(self, game: Game, process, owner):
         Gun.__init__(self, game, process,
                      game.getTextureManager().getAnimationPack(AnimationPackInfo.USUALGUN_BULLET_ANIMATION),
-                     process.getFactory().createRectangleBodyTemplate(b2_kinematicBody, 10, 10),
+                     createRectangleBodyTemplate(b2_kinematicBody, 10, 10),
                      game.getTextureManager().getAnimationPack(AnimationPackInfo.USUALGUN_ANIMATION),
                      {"bulletSpeed": 800, "bulletType": "OneDirection"}, owner)
         self.cooldown = 0.3
@@ -19,7 +20,7 @@ class BallisticGun(Gun):
     def __init__(self, game: Game, process, owner):
         Gun.__init__(self, game, process,
                      game.getTextureManager().getAnimationPack(AnimationPackInfo.BALLISTICGUN_BULLET_ANIMATION),
-                     process.getFactory().createRectangleBodyTemplate(b2_dynamicBody, 10, 10),
+                     createRectangleBodyTemplate(b2_dynamicBody, 10, 10),
                      game.getTextureManager().getAnimationPack(AnimationPackInfo.BALLISTICGUN_ANIMATION),
                      {"bulletSpeed": 800, "bulletType": "Ballistic"}, owner)
         self.cooldown = 0.7
