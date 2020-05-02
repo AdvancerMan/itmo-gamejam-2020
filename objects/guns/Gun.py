47eff22aa9604921a994136b4d697b0082c302bc
AdvancerMan <advancerman@gmail.com>
2020-05-02 10:00:17 +0700

    Fixed Gun and wrote some notes

--- objects/guns/Gun.py
+++ objects/guns/Gun.py
@@ -2,6 +2,7 @@ import pyganim as pga
 from Box2D import *
 from game.Game import Game
 from objects.base.InGameObject import InGameObject
+from util.box2d.BodyFactory import BodyTemplate
 
 
 class Bullet(InGameObject):
@@ -13,18 +14,33 @@ class Bullet(InGameObject):
 
 
 class Gun:
-    def __init__(self, game: Game, process, bulletanim: pga.PygAnimation, bulletbody: b2Body,
-                 gunanim: pga.PygAnimation, gunbody: b2Body, params: dict):
+    """
+    :NOTE:
+    Gun shouldn't have body, body is a physical object
+    which is used to compute collisions.
+    Gun shouldn't collide with any objects.
+    (Probably that strange object on (0, 0) was gun body)
+
+    If you create body and reuse it in Bullet
+    you would get many objects on the same body
+    so you should use something like body template
+
+    Variables should be named in camelCase or i_dont_know_name_of_this_case
+    """
+
+    def __init__(self, game: Game, process, bulletAnim: pga.PygAnimation,
+                 bulletBodyTemplate: BodyTemplate,
+                 gunAnim: pga.PygAnimation, params: dict):
         # process: GameProcess
         self.__game = game
         self.__process = process
         self.__params = params      # params = {"bulletSpeed"}
                                     # maybe add "deviation" and "angle"
-        self.__bulletAnimation = bulletanim
-        self.__gunAnimation = gunanim
-        self.__bulletBody = bulletbody
-        self.__gunBody = gunbody
+        self.__bulletAnimation = bulletAnim
+        self.__gunAnimation = gunAnim
+        self.__bulletBody = bulletBodyTemplate
 
     def spawnBullet(self):
         self.__process.addObject(Bullet(self.__game, self.__process, self.__bulletAnimation,
-                                        self.__params['bulletSpeed'], self.__bulletBody))
+                                        self.__params['bulletSpeed'],
+                                        self.__bulletBody.createBody(self.__process.getFactory())))
--- objects/guns/UsualGun.py
+++ objects/guns/UsualGun.py
@@ -9,7 +9,6 @@ class UsualGun(Gun):
     def __init__(self, game: Game, process):
         Gun.__init__(self, game, process,
                      game.getTextureManager().getAnimation(AnimationInfo.USUALGUN_BULLET_ANIMATION),
-                     process.getFactory().createRectangleBody(b2_kinematicBody, 10, 10),
+                     process.getFactory().createRectangleBodyTemplate(b2_kinematicBody, 10, 10),
                      game.getTextureManager().getAnimation(AnimationInfo.USUALGUN_ANIMATION),
-                     process.getFactory().createRectangleBody(b2_staticBody, 100, 10),
                      {"bulletSpeed": 200})
