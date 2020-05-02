bc64d37557b6e86c72c5d2fd25291cdf8cc98317
AdvancerMan <advancerman@gmail.com>
2020-05-02 19:09:19 +0700

    Upgraded Builder to build many types of platforms

--- levels/LevelBuilder.py
+++ levels/LevelBuilder.py
@@ -2,6 +2,7 @@ from game.Game import Game
 import json
 from os.path import join
 
+from objects.platforms.HalfCollidedPlatform import HalfCollidedPlatform
 from objects.platforms.SolidPlatform import SolidPlatform
 
 
@@ -14,7 +15,13 @@ class Builder:
     def __init__(self, game: Game):
         # process: GameProcess
         self.__game = game
+        self.__objects = {
+            "solid": SolidPlatform,
+            "halfCol": HalfCollidedPlatform
+        }
 
     def build(self, process, levelName: str):
-        for plate in loadLevel(levelName)["platforms"]:
-            process.addObject(SolidPlatform(self.__game, process, plate["x"], plate["y"], plate["width"], plate["height"]))
+        for class_name, objs in loadLevel(levelName).items():
+            Constructor = self.__objects[class_name]
+            for kwargs in objs:
+                process.addObject(Constructor(self.__game, process, **kwargs))
