4e56a36916c95c642b5b10c94b6550c5bdd540a5
AdvancerMan <advancerman@gmail.com>
2020-05-02 14:49:07 +0700

    Fixed jumps. Now player can't fly

--- objects/base/ActiveObject.py
+++ objects/base/ActiveObject.py
@@ -3,6 +3,7 @@ from Box2D import *
 from game.Game import Game
 from objects.base.InGameObject import InGameObject
 from objects.guns.UsualGun import UsualGun
+from util.FloatCmp import lessOrEquals
 
 
 class ActiveObject(InGameObject):
@@ -12,6 +13,7 @@ class ActiveObject(InGameObject):
         InGameObject.__init__(self, game, process, animation, body)
         self.__speed = speed
         self.__jumpPower = jumpPower
+        self.__grounds = set()
         self.__gun = UsualGun(game, process)
 
     def go(self, speed):
@@ -24,8 +26,19 @@ class ActiveObject(InGameObject):
         self.go(self.__speed)
 
     def jump(self):
-        self.getBody().linearVelocity = b2Vec2(self.getBody().linearVelocity.x, self.__jumpPower)
+        if self.isOnGround():
+            self.getBody().linearVelocity = b2Vec2(self.getBody().linearVelocity.x, self.__jumpPower)
 
     def shoot(self):
         self.__gun.spawnBullet()
-        pass
+
+    def isOnGround(self):
+        return len(self.__grounds) > 0
+
+    def beginContact(self, obj, contact: b2Contact):
+        aabb = (self.getAABB(), obj.getAABB())
+        if lessOrEquals(aabb[1].y + aabb[1].h, aabb[0].y):
+            self.__grounds.add(obj)
+
+    def endContact(self, obj, contact: b2Contact):
+        self.__grounds.discard(obj)
