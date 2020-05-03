5618d4f7ffb519caa8970fbe849e3ec517777476
FluFFka <iliasfedorenko@mail.ru>
2020-05-04 01:25:03 +0300

    Added music (uporoto)

--- process/MenuProcess.py
+++ process/MenuProcess.py
@@ -2,15 +2,25 @@ import pygame as pg
 from game.Game import Game
 from process.Process import Process
 from process.GameProcess import GameProcess
+from os.path import join
+
+
+def _createPath(*picPath: str) -> str:
+    return join(*picPath)
 
 
 class MenuProcess(Process):
     def __init__(self, game: Game):
         self.__game = game
+        pg.mixer.init()
+        pg.mixer.music.load(_createPath('music', 'main_menu.mp3'))
+        pg.mixer.music.play()
 
     def processEvents(self, events: list):
         for e in events:
             if e.type == pg.QUIT:
+                pg.mixer.music.stop()
                 self.__game.stop()
             if e.type == pg.KEYDOWN:
+                pg.mixer.music.stop()
                 self.__game.addProcess(GameProcess(self.__game))
