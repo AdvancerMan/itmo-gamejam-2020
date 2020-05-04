008af047a36dcd5e56a39e3fb2c5c7be2b620bf4
AdvancerMan <advancerman@gmail.com>
2020-05-04 12:14:02 +0700

    Added health bar to anthill

--- objects/guns/Gun.py
+++ objects/guns/Gun.py
@@ -108,7 +108,7 @@ class Bullet(InGameObject):
         if self.__dead:
             self.__process.addObject(Explode(self.__game, self.__process,
                                              self.__params["ExplodeAnimation"],
-                                             self.__process.getFactory().createRectangleBody(b2_staticBody,
+                                             self.__process.getFactory().createRectangleBody(b2_dynamicBody,
                                              self.__params["ExplodeSize"], self.__params["ExplodeSize"], gravityScale=0),
                                              self.getPosition(), self.__params))
             self.process.removeObject(self)
